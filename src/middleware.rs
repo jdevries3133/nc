a87f98ba485b0ed6546feec53fd264b94903eed2
Jack DeVries <jdevries3133@gmail.com>
2023-09-16 16:27:17 -0400

    fix: hx-boost bfcache bug
    Navigating back from an external link shows the response from the last
    AJAX request, a partial page. We'll tell the browser not to cache HTML
    responses. See also https://github.com/bigskysoftware/htmx/issues/724

--- src/middleware.rs
+++ src/middleware.rs
@@ -1,6 +1,6 @@
 use super::errors::ServerError;
 use axum::{
-    http::{HeaderValue, Request},
+    http::{HeaderMap, HeaderValue, Request},
     middleware::Next,
     response::Response,
 };
@@ -11,12 +11,22 @@ pub async fn html_headers<B>(
 ) -> Result<Response, ServerError> {
     let mut response = next.run(request).await;
     let headers = response.headers_mut();
-    if let Some(h) = headers.get("content-type") {
-        if h.to_str().expect("header is ascii") == "text/plain; charset=utf-8" {
+
+    // Set content-type to text/html unless otherwise specified
+    if let Some(content_type) = headers.get("content-type") {
+        if content_type.to_str().expect("header is ascii") == "text/plain; charset=utf-8" {
             headers.remove("content-type");
             headers.insert("content-type", HeaderValue::from_str("text/html").expect("text/html is ascii"));
         }
     }
+    // Set Cache-Control: no-cache unless otherwise specified. Most endpoints
+    // return HTML interpolated with user data which is liable to change all
+    // the time, so we don't want these responses to be cached. At least one
+    // route, though, does have some specific cache-control. The route to serve
+    // static JS can be cached forever.
+    if !headers.contains_key("cache-control") {
+        headers.insert("cache-control", HeaderValue::from_str("no-cache").expect("no-cache is ascii"));
+    };
 
     Ok(response)
 }
